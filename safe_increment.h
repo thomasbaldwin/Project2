c8721f48b0a82a438c95b22078c82643e4e460d4
Thomas Baldwin <thomasbaldwinj@gmail.com>
2016-03-24 15:00:47 -0400

    Changes

--- safe_increment.h
+++ safe_increment.h
@@ -2,8 +2,8 @@
 #define SAFE_INCREMENT_H
 
 void increment(char*);
-void enter_region(void);
-void leave_region(void);
+int enter_region(void);
+void leave_region(int);
 int get_set_flag(pid_t);
 int get_turn(pid_t);
 
